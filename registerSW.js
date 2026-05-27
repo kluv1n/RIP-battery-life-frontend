12febe72474a71ae0e87a679b5dee8fae0ba9a9b
Klim Kryzhnyi <dert97975@gmail.com>
2026-05-27 03:25:49 +0300

    Updates

--- registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/RIP-battery-life-frontend/sw.js', { scope: '/RIP-battery-life-frontend/' })})}
